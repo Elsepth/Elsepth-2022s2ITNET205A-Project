c1e0e95eb5ae5534d80facc7d94e4f5ed98ffa85
Lily <nacaratflame@gmail.com>
2022-09-23 11:51:47 +1000

    wip

--- ui_pyqt.py
+++ ui_pyqt.py
@@ -41,54 +41,122 @@ import fetch
 
 
 # https://www.pythonguis.com/tutorials/pyqt6-signals-slots-events/
-class MainWindow2(QMainWindow): #extends QMainWindow
+
+#this class extends QMainWindow, so it includes all methods of QMainWindow by default
+class MainWindow2(QMainWindow):
 	def __init__(self):
-		super().__init__() #calls __init__ from QMainWindow
+		#call __init__ from QMainWindow
+		super().__init__() 
+		
+		#set the window title
 		self.setWindowTitle("MapQuest Again")
 	
-		self.label = QLabel("Enter locations to find directions between:")
+		#a text field
+		self.label_instruction = QLabel("Enter locations to find directions between:")
+		self.label_output = QLabel("OUTPUT")
 
+		#two input fields
 		self.formOrig = QLineEdit()
 		self.formOrig.setPlaceholderText("From")
+		self.formOrig.textChanged.connect(self.slot_update)
+		self.formOrig.setStatusTip("<TODO>")
+
+
 		self.formDest = QLineEdit()
 		self.formDest.setPlaceholderText("To")
+		self.formDest.textChanged.connect(self.slot_update)
+		self.formDest.setStatusTip("<TODO>")
 
 
 
+		# this button does not need to store a value
+		button_defaults = QPushButton("Defaults")
+		button_defaults.clicked.connect(self.slot_button_defaults)
+		button_defaults.setStatusTip("<TODO>")
 
-	#	button_defaults = QPushButton("Defaults")
 
+		# this button stores a value, so it must be self.button
 		self.button_run = QPushButton("Run")
-	#	self.button_run.setEnabled(False)
+		# disable button by default
+		self.button_run.setEnabled(False)
+		# sends a SIGNAL on click, which connects to a slot
 		self.button_run.clicked.connect(self.slot_button_run)
-		# this sends a SIGNAL on click, which connects to a slot
+		# displays status on hover
+		self.button_run.setStatusTip("Get directions")
+
+
+	#the layout definition
+		# layout for user input
+		layout_input = QVBoxLayout()
+		layout_input.addWidget(self.label_instruction)
+		layout_input.addWidget(self.formOrig)
+		layout_input.addWidget(self.formDest)
+
+		# buttons are combined in a horizontal row
+		layout_buttons = QHBoxLayout()
+		layout_buttons.addWidget(button_defaults)
+		layout_buttons.addWidget(self.button_run)
+
+		layout_input.addLayout(layout_buttons)
+
+
+		# layout for program output
+		layout_output = QVBoxLayout()
+		layout_output.addWidget(self.label_output)
 
 		layout = QVBoxLayout()
-		layout.addWidget(self.label)
-		layout.addWidget(self.formOrig)
-		layout.addWidget(self.formDest)
-	#	layout.addWidget(self.button_defaults)
-		layout.addWidget(self.button_run)
+		layout.addLayout(layout_input)
+		layout.addLayout(layout_output)
 
 		container = QWidget()
 		container.setLayout(layout)
 
 		self.setMinimumSize(QSize(400,300))
-	#	self.setMaximumSize(QSize(1000,1000))
+		self.setMaximumSize(QSize(1000,1000))
 
 	#	self.setCentralWidget(self.button_run)
 		self.setCentralWidget(container)
 
+		statusBar = QStatusBar(self)
+		self.setStatusBar(statusBar)
+
+		statusBar.showMessage("Ready")
+
+
 	#Slots, these accept signals
+	def slot_button_defaults(self):
+		print("DEFAULT")
+		self.formOrig.setText("Washington, D.C.")
+		self.formDest.setText("Baltimore, Md")
+
 	def slot_button_run(self):
 		print("RUN")
+		
+		orig = self.formOrig.text()
+		dest = self.formDest.text()
 
+	#	self.d = fetch.Directions(orig,dest)
+
+		self.label_output.setText (
+			func_output	(
+				orig,
+				dest,
+				fetch.Directions(orig,dest),
+				"metric"
+			)
+		)
+
+	#	self.setOutput("FOO BAR")
 
 
 	def slot_update(self):
 		#if both fields are filled then enable run
-	#	self.button_run.setEnabled(True)
-		pass
+		if "" == self.formOrig.text(): 
+			self.button_run.setEnabled(False)
+		elif "" == self.formDest.text(): 
+			self.button_run.setEnabled(False)
+		else: 
+			self.button_run.setEnabled(True)
 
 
 class MainWindow(QWidget):
@@ -244,6 +312,59 @@ class MainWindow(QWidget):
 
 		return 0
 
+
+def func_output(orig,dest,fetched,unit_system):
+	print("OUTPUTTING")
+	status = fetched.json_status
+	data = fetched.json_data
+
+	if status == 0:
+		print("JSON STATUS 0")
+
+		o = ("API Status: " + str(status) + " = A successful route call."
+		+ "\n============================================="
+		+ "\nDirections from " + (orig) + " to " + (dest)
+		+ "\nTrip Duration: " + (data["route"]["formattedTime"])
+		)
+		
+		if "metric" == unit_system:
+			o = o + ("\nKilometers: " + str("{:.2f}".format((data["route"]["distance"])*1.61))
+			+ "\nFuel Used (Ltr): " + str("{:.2f}".format((data["route"]["fuelUsed"])*3.78))
+			)
+		elif "imperial" == unit_system:
+			o = o + ("\nMiles: " + str("{:.2f}".format((data["route"]["distance"])))
+			+ "\nFuel Used (Gal): " + str("{:.2f}".format((data["route"]["fuelUsed"])))
+			)
+		o += "\n================== DIRECTIONS =================="
+
+		for each in data["route"]["legs"][0]["maneuvers"]:
+			if "metric" == unit_system:
+				dist = " (" + str("{:.2f}".format((each["distance"])*1.61)) + " km)"
+			elif "imperial" == unit_system:
+				dist = " (" + str("{:.2f}".format((each["distance"]))) + " mi)"
+			e = ("\n⤷ " 
+			+ (each["narrative"]) 
+			+ dist
+			#+ "\n============================================="
+			)
+
+			o = o + e
+
+	elif status == 402:
+		o=("\n**********************************************"
+		+ "\nStatus Code: " + str(status) + "; Invalid user inputs for one or both locations."
+		+ "\n**********************************************")
+	elif status == 611:
+		o=("\n**********************************************"
+		+ "\nStatus Code: " + str(status) + "; Missing an entry for one or both locations."
+		+ "\n**********************************************")
+	else:
+		o=("\n************************************************************************"
+		+ "\nFor Status Code: " + str(status) + "; Refer to:"
+		+ "\nhttps://developer.mapquest.com/documentation/directions-api/status-codes"
+		+ "\n************************************************************************")
+
+	return o
 
 def main():
 	app = QApplication(sys.argv)
